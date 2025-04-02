be3b1ed33b789d8058b37c2403140d14e238080d
Aleksander <sanya.slepow2015@yandex.ru>
2025-04-02 16:11:15 +0500

    Commit20

--- phone_auth_service/phone_auth_service/settings.py
+++ phone_auth_service/phone_auth_service/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+
     }
 }
 
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
+
+AUTH_USER_MODEL = 'users.User'
